f564edef8601916bf9ee2bfcd81f7a8ba90e6d85
sanket <spatil270403@gmail.com>
2024-01-17 18:06:45 +0530

    modified single quote to backticks

--- Express Node/MongoDB/creating_DB.js
+++ Express Node/MongoDB/creating_DB.js
@@ -2,7 +2,7 @@
 
 const mongoose = require("mongoose");
 mongoose.connect("mongodb+srv://admin:KUOMZ3v7gfyPGlVA@cluster0.dikomqc.mongodb.net/userappnew");
-// connect with mongodb compass, '/userappnew' at last is the database that we trying to access
+// connect with mongodb compass, `/userappnew` at last is the database that we trying to access
 
 // design schema for database: `Users` is the table name
 const User = mongoose.model('Users', {name: String, email: String, password: String});
