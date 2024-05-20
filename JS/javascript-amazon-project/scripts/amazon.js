3ef0e14b73ea3878a4e74a8ca5da36f31de6063b
sanket <spatil270403@gmail.com>
2024-05-20 21:25:41 +0530

    used modules and import export

--- JS/javascript-amazon-project/scripts/amazon.js
+++ JS/javascript-amazon-project/scripts/amazon.js
@@ -1,3 +1,4 @@
+import { cart } from "../data/cart.js";
 
 let productsHTML = "";
 
@@ -76,6 +77,12 @@ document.querySelectorAll(".js-add-to-cart").forEach(button => {
                 quantity: 1
             });
         }
-        console.log(cart);
+
+        let cartQuantity = 0;
+        cart.forEach((item) => {
+            cartQuantity += item.quantity;
+        })
+
+        document.querySelector(".cart-quantity").innerHTML = cartQuantity;
     })
 });
