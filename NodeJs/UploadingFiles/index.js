adb20b5e0d774726705fa10a4325e0b5e931fbaa
sanket <spatil270403@gmail.com>
2024-05-25 19:04:50 +0530

    added diskStorage so user uploaded files can be viewed

--- NodeJs/UploadingFiles/index.js
+++ NodeJs/UploadingFiles/index.js
@@ -7,7 +7,25 @@ const PORT = 3000;
 const app = express();
 
 // whatever user will upload from frontEnd keep it in 'uploads/' folder (upload is a middleware basically)
-const upload = multer({ dest: 'uploads/' });
+// const upload = multer({ dest: 'uploads/' });
+
+// now we don't need above upload instance coz we are enable to open the uploaded files!!
+// so we create disk storage. The disk storage engine gives you full control on storing files to disk.
+// it has 2 options that determines where the file should b estored.
+const storage = multer.diskStorage({
+    // req: original request object, file: file which user trying to upload, cb: callback fn it has 2 params 1. error 2. name of folder destination
+    destination: function(req, file, cb) {
+        return cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+        // we can put original file name from file.originalname but it can override if same name file uploaded by other user
+        // so we make our own fileName by giving it prefix of unique date
+        return cb(null, `${Date.now()}-${file.originalname}`)
+    }
+})
+
+// now we make upload
+const upload = multer({ storage: storage });
 
 // ejs setting
 app.set("view engine", "ejs");
